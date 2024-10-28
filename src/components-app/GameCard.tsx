6801d372695c76fee87debd13fc6a2a905e9cafc
Mouhammad Ndiaye <mouhamedn842@gmail.com>
2024-10-28 17:51:27 +0000

    Displaying Icons

--- src/components-app/GameCard.tsx
+++ src/components-app/GameCard.tsx
@@ -1,5 +1,7 @@
+
 import { Game } from "@/hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIcons from "./PlatformIcons";
 
 interface Props {
   game: Game;
@@ -9,7 +11,8 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow={"hidden"}>
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIcons platforms={game.parent_platforms.map(p => p.platform )}/>
       </CardBody>
     </Card>
   );
