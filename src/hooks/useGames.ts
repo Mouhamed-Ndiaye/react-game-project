a7195a37b2cc5a41222f81359e031bdc7ea043ef
Mouhammad Ndiaye <mouhamedn842@gmail.com>
2024-10-29 09:39:27 +0000

    Loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: { platform: Platform }[]
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface GameAttributes {
@@ -23,23 +24,29 @@ interface GameAttributes {
 
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+    const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false)
 
     useEffect(() => {
         const controller = new AbortController();
 
-    apiClient
-      .get<GameAttributes>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-        });
+        setLoading(true);
+        apiClient
+            .get<GameAttributes>("/games", { signal: controller.signal })
+            .then((res) => {
+                setGames(res.data.results);
+                setLoading(false);
+            })
+            .catch((err) => {
+                if (err instanceof CanceledError) return;
+                setError(err.message)
+                setLoading(false);
+            });
       
         return () => controller.abort();
   }, []);
     
-    return { games, error }
+    return { games, error, isLoading }
 }
 
 export default useGames;
