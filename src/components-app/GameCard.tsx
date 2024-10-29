bd560b2a74ac34d3cf32a1bfafa30d09c86ed4e7
Mouhammad Ndiaye <mouhamedn842@gmail.com>
2024-10-29 11:34:29 +0000

    Refactor duplicated styles in GameCard and GameGrid

--- src/components-app/GameCard.tsx
+++ src/components-app/GameCard.tsx
@@ -8,11 +8,13 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow={"hidden"}>
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIcons platforms={game.parent_platforms.map(p => p.platform )}/>
+        <PlatformIcons
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
